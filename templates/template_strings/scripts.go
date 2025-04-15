a0bad545a235b01b80964b602b8b77814a0e1cca
Claudio <claucondor@gmail.com>
2025-04-14 22:42:23 -0400

    Modifica la función main en scripts.go para cambiar su acceso a 'view' y mejora la forma de obtener la referencia al vault utilizando 'borrow' desde 'capabilities', optimizando así la claridad y el manejo de la capacidad del token fungible.

--- templates/template_strings/scripts.go
+++ templates/template_strings/scripts.go
@@ -4,11 +4,12 @@ const GenericFungibleBalance = `
 import FungibleToken from "./FungibleToken.cdc"
 import TOKEN_DECLARATION_NAME from TOKEN_ADDRESS
 
-pub fun main(account: Address): UFix64 {
+access(all)
+view fun main(account: Address): UFix64 {
 
     let vaultRef = getAccount(account)
-        .getCapability(TOKEN_BALANCE)
-        .borrow<&TOKEN_DECLARATION_NAME.Vault{FungibleToken.Balance}>()
+        .capabilities
+        .borrow<&TOKEN_DECLARATION_NAME.Vault{FungibleToken.Balance}>(TOKEN_BALANCE)
         ?? panic("failed to borrow reference to vault")
 
     return vaultRef.balance
